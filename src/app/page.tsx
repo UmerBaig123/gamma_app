a22f1700003607b477ca990258bcbee9e8f88977
umerbaig9922 <muhammadumerbaig9922@gmail.com>
2025-03-06 14:32:01 +0500

    Creates main page containing aggrid and crudButtons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,176 @@
-import Image from "next/image";
+"use client";
+import React, { useState, useEffect, useRef, createContext } from "react";
+import { AgGridReact } from "ag-grid-react";
+import styles from "@/app/page.module.css";
+import { Button } from "@/components/ui/button";
+import { _setAriaColSpan, themeMaterial } from "ag-grid-community";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import CrudButtons from "@/components/CrudButtons";
+import axios from "axios";
+import { AlertDestructive } from "@/components/ErrorAlert";
 
+export const columnsContext = createContext(null);
+ModuleRegistry.registerModules([AllCommunityModule]);
 export default function Home() {
+  const gridRef = useRef<AgGridReact>(null);
+  const [rowData, setRowData] = useState<any>([]);
+  const [error, setError] = useState<string>("");
+  const [columnDefs] = useState<any>([
+    {
+      field: "ticker",
+      colId: "ticker",
+      headerName: "Ticker",
+      sortable: true,
+      filter: true,
+      pinned: "left",
+    },
+    {
+      field: "timestamp",
+      headerName: "Timestamp",
+      sortable: true,
+      filter: true,
+      pinned: "left",
+    },
+    {
+      field: "ask_price",
+      headerName: "Ask Price",
+      sortable: true,
+      filter: true,
+    },
+    {
+      field: "bid_price",
+      headerName: "Bid Price",
+      sortable: true,
+      filter: true,
+    },
+    {
+      field: "close_price",
+      headerName: "Close Price",
+      sortable: true,
+      filter: true,
+    },
+    {
+      field: "volume",
+      headerName: "Volume",
+      sortable: true,
+      filter: true,
+    },
+    {
+      field: "vva",
+      headerName: "VVA",
+      sortable: true,
+      filter: true,
+    },
+  ]);
+  useEffect(() => {
+    axios
+      .get("/trades")
+      .then((response) => {
+        console.log("Trades fetched successfully:", response.data);
+        setRowData(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the trades:", error);
+      });
+  }, []);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div className={styles.crud_buttons}>
+          <columnsContext.Provider value={columnDefs}>
+            <CrudButtons
+              error={error}
+              setError={setError}
+              grid={gridRef}
+              onAdd={(data: object) => {
+                axios
+                  .post("/trades", data)
+                  .then((response) => {
+                    console.log("Trade added successfully:", response.data);
+                    setRowData((prevData: []) => [...prevData, response.data]);
+                  })
+                  .catch((error) => {
+                    console.error(
+                      "There was an error adding the trade:",
+                      error
+                    );
+                  });
+              }}
+              onDelete={() => {
+                const selectedRows = gridRef.current?.api.getSelectedRows();
+                if (!selectedRows?.length) {
+                  setError("Please select a row to delete");
+                  return;
+                }
+                selectedRows?.forEach((row) => {
+                  setRowData((prevData: []) =>
+                    prevData.filter((data: any) => data._id !== row._id)
+                  );
+                  axios
+                    .delete(`/trades/${row._id}`)
+                    .then((response) => {
+                      console.log("Trade deleted successfully:", response.data);
+                    })
+                    .catch((error) => {
+                      console.error(
+                        "There was an error deleting the trade:",
+                        error
+                      );
+                    });
+                });
+              }}
+              onEdit={(data: any) => {
+                axios
+                  .put(`/trades/${data._id}`, data)
+                  .then((response) => {
+                    console.log("Trade edited successfully:", response.data);
+                    setRowData((prevData: []) =>
+                      prevData.map((d: any) =>
+                        d._id === response.data._id ? response.data : d
+                      )
+                    );
+                  })
+                  .catch((error) => {
+                    console.error(
+                      "There was an error editing the trade:",
+                      error
+                    );
+                  });
+              }}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </columnsContext.Provider>
+        </div>
+        <div
+          className="aggrid-container"
+          style={{ width: "90vw", height: "500px" }}
+        >
+          <AgGridReact
+            ref={gridRef}
+            rowData={rowData}
+            columnDefs={columnDefs}
+            theme={themeMaterial}
+            domLayout="autoHeight"
+            animateRows={true}
+            pagination={true}
+            rowSelection={"multiple"}
+            rowMultiSelectWithClick={true}
+            paginationPageSize={5}
+            gridOptions={{
+              autoSizeStrategy: {
+                type: "fitGridWidth",
+                defaultMinWidth: 100,
+                columnLimits: [
+                  {
+                    colId: "ticker",
+                    maxWidth: 150,
+                  },
+                ],
+              },
+            }}
+          />
+          {error && <AlertDestructive message={error} />}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
